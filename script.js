8ecbfe596b33383845f2fbc68b82416c4fcd694f
Calpico <Calpico@users.noreply.github.com>
2024-10-31 14:38:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*// Theme toggle functionality
-const toggleBtn = document.getElementById('theme-toggle-btn');
-const currentTheme = localStorage.getItem('theme') || 'light';
-
-document.documentElement.setAttribute('data-theme', currentTheme);        
-
-toggleBtn.textContent = currentTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
-
-toggleBtn.addEventListener('click', () => {
-    let theme = document.documentElement.getAttribute('data-theme');
-    if (theme === 'light') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        toggleBtn.textContent = 'Switch to Light Mode';
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        toggleBtn.textContent = 'Switch to Dark Mode';
-    }
-});*/
-
-
     const toggle = document.getElementById('toggle');
     const body = document.body;
 
